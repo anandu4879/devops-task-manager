6ed68995c3273f02bebf54931e4d56b86f5de9e1
anandu4879 <anandu4879@gmail.com>
2026-07-06 19:47:42 +0530

    use S3 native locking instead of DynamoDB

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -10,14 +10,15 @@ terraform {
 
   # Create the S3 bucket + DynamoDB table manually ONCE before using this
   # backend (see README "Bootstrap remote state" section), then uncomment:
-  #
-  # backend "s3" {
-  #   bucket         = "your-unique-tfstate-bucket-name"
-  #   key            = "task-manager/terraform.tfstate"
-  #   region         = "us-east-1"
-  #   dynamodb_table = "terraform-locks"
-  #   encrypt        = true
-  # }
+
+  backend "s3" {
+     bucket         = "anandu-devops-tfstate-2026"
+     key            = "task-manager/terraform.tfstate"
+     region         = "ap-south-1"
+     #dynamodb_table = "terraform-locks"
+     use_lockfile = true
+     encrypt        = true
+   }
 }
 
 provider "aws" {
